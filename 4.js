87e27da6200c0da23da5c5e5e236de6bb5c715b1
Kamalakkannan M <kamalakkannanmanoharan@gmail.com>
2025-12-12 16:00:06 +0530

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,7 @@
+const changeColorBtn = document.getElementById('change-color-btn');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
+  document.body.style.backgroundColor = randomColor;
+});
